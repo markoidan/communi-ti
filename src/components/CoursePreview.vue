a4fccf63c436b4940696a705b08d686314919f0c
Jenny Kolmanovitch <jenny.kolmanovitch@tipalti.com>
2023-02-13 13:44:03 +0200

    courses

---
+++ src/components/CoursePreview.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="course-preview">    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CoursePreview",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped></style>
