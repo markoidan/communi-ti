398258fe9ba024b03b88cf2cdcf7dc8cee98366f
TIPALTI\narkis.daniel <TIPALTI\narkis.daniel>
2023-02-13 14:45:24 +0200

    create avatar

--- src/components/SpeakerMetadata.vue
+++ src/components/SpeakerMetadata.vue
@@ -1,29 +1,40 @@
 <template>
-    <div class="speaker-metadata">
-        <v-avatar image="../assets/avatar.jpg" :size="iconSize"></v-avatar>
-      <div class="speaker-metadata__description">
-        <span>{{ name }}</span>
-        <span>{{ description }}</span>
-      </div>
+  <div class="speaker-metadata">
+    <v-avatar image="src/assets/avatar.png" :size="iconSize"></v-avatar>
+    <div class="speaker-metadata__description">
+      <span>{{ name }}</span>
+      <span>{{ description }}</span>
     </div>
+  </div>
 </template>
 
 <script>
-
 export default {
   name: "Speaker-Metadata",
   props: {
     iconSize: {
       type: Number,
-      default: 45
+      default: 45,
     },
     name: {
       type: String,
-      require: true
+      require: true,
     },
     description: {
       type: String,
-      require: true
-    }
-  }
+      require: true,
+    },
+  },
+};
+</script>
+<style scoped>
+.speaker-metadata {
+  display: flex;
+  flex-direction: row;
 }
+.speaker-metadata__description {
+    display: flex;
+    flex-direction: column;
+    margin-left: 10px;
+}
+</style>
