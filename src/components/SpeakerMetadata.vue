3565a5af50c2ec27545926f696a359559cea789b
TIPALTI\narkis.daniel <TIPALTI\narkis.daniel>
2023-02-13 14:07:28 +0200

    create avatar component

---
+++ src/components/SpeakerMetadata.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="speaker-metadata">
+        <v-avatar image="../assets/avatar.jpg" :size="iconSize"></v-avatar>
+      <div class="speaker-metadata__description">
+        <span>{{ name }}</span>
+        <span>{{ description }}</span>
+      </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: "Speaker-Metadata",
+  props: {
+    iconSize: {
+      type: Number,
+      default: 45
+    },
+    name: {
+      type: String,
+      require: true
+    },
+    description: {
+      type: String,
+      require: true
+    }
+  }
+}
