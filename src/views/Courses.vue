bd77d5831c9059d34f30a4b0d6552c6ebbd3b280
idan.marko <idan.marko@tipalti.com>
2023-02-13 15:44:40 +0200

    add filters

--- src/views/Courses.vue
+++ src/views/Courses.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="courses">
-    <h1>Courses</h1>
-    <event></event>
+    <h1>Events</h1>
+    <filter-events-row></filter-events-row>
+    <CoursePreview></CoursePreview>
   </div>
 </template>
 <script>
 import CoursePreview from "../components/CoursePreview.vue";
+import FilterEventsRow from "@/components/FilterEventsRow.vue";
 export default {
   name: "CoursesView",
-  components: [CoursePreview],
+  components: { CoursePreview, FilterEventsRow },
 };
 </script>
